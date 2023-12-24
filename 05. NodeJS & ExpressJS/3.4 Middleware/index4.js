917ae20d019f6dce4767e20119392cb915fdcd81
Bhuvana08 <bhuvaneswarikodati@gmail.com>
2023-12-24 12:49:56 +0530

    NodeJS and Express modules with Secrets Access Project

---
+++ 05. NodeJS & ExpressJS/3.4 Middleware/index4.js
@@ -0,0 +1,31 @@
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+import morgan from "morgan";
+
+const app = express();
+const port = 3000;
+var bandName = "";
+
+app.use(express.urlencoded({extended: true}));
+
+function bandNameGenerator(req,res,next){
+  console.log(req.body);
+  bandName = req.body["street"]+req.body["pet"];
+  next();
+}
+
+app.use(bandNameGenerator);
+
+app.post("/submit",(req,res) => {
+  res.send(`<h1> Your band name is : </h1><p>${bandName}⭐ </p>`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
